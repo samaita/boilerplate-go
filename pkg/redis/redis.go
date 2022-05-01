187cee4fadc6e59a2efcd6e16f77bffae2bd74a9
Samaita <garysamaita@gmail.com>
2022-05-01 11:35:12 +0700

    [feat] Adding Redis Connection (#3)
    * [feat] Adding config redis
    
    * [feat] Adding redis init connection flow
    
    * [feat] Update go.mod & go.sum
    
    * [feat] Adding cache.go to initiate redis connection

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,3 +1,24 @@
 package redis
 
-type Redis struct{}
+import (
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Redis struct {
+	DB       int
+	Host     string
+	Password string
+	Port     string
+	Timeout  time.Duration
+}
+
+// Connect create a new SQLX connection
+func (rds *Redis) Connect() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     rds.Host + ":" + rds.Port,
+		Password: rds.Password,
+		DB:       rds.DB,
+	})
+}
